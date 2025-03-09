7d7591beb5dca3f73769eb1213c264cf9e0bcf4b
GZod01 <gzod01@gzod01.fr>
2025-03-09 19:39:41 +0100

    s

--- index.php
+++ index.php
@@ -174,7 +174,6 @@ if (isset($_GET["admin"])) {
                         die("WARNING ! score time is before start time <a href='?event_code=$event_code&admin=score&sub_event_code=$sub_event_code'>refresh</a>");
                     }
                     $score = 0;
-                    $timelength=0;$position_in_classment=0;$bestscore=0;
                     if($is_speedrun){
                         //TODO: check this if code block when implementing "http request speedrun" (for dev speedrun or other things that can be complete with a special http request)
                         // SPEEDRUN SCORE CALCULATION
